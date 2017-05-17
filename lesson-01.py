bd647269cbc5c5039f78a5a40b90707d084adad1
Vlad <thunderinthehallway@gmail.com>
2017-05-17 19:41:57 +0300

    Первая лекция

---
+++ lesson-01.py
@@ -0,0 +1,14 @@
+who = 'Python'
+print('Hello, ',who)
+i1 = 1
+f1 = 1.2
+print(i1,f1)
+b1 = True 
+d1 = {
+    'id':1,
+    'fio':'Ivan',
+    'is_developer':True,
+    'skills':['python']
+}
+print(d1['fio'], d1['skills'][0])
+print(type(i1) , type(f1))
