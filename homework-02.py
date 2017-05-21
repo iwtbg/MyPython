c19fc80f1c80a94e410d14ee7245183352e6849b
Vlad <thunderinthehallway@gmail.com>
2017-05-21 21:53:58 +0300

    commiting HM

---
+++ homework-02.py
@@ -0,0 +1,10 @@
+Dishes = int(input("Введите количество тарелок: "))
+Cleanser = float(input("Введите количество моющего средства: "))
+
+while Dishes >= 1 and Cleanser >=0.5:
+    Dishes -= 1
+    Cleanser -= 0.5
+    print("Осталось вымыть ", Dishes, " тарелок")
+    print("Осталось моющего средства: ", Cleanser, "мл")
+
+print("На момент окончания мытья посуды осталось ", Dishes, "тарелок и", Cleanser, "мл моющего средства")
